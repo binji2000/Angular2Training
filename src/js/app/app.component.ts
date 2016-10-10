86db5125909f3005d90f36dc4b93d37322319519
Bin Ji <bin.ji@wolterskluwer.com>
2016-10-10 12:45:52 -0400

    this is second commit

--- src/js/app/app.component.ts
+++ src/js/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-//import { BrowserModule } from '@angular/platform-browser';
-//import { AppComponent } from './app.component';
 
+import '../../css/styles.scss';
+
+@Component({
+	selector: 'my-app',
+	template: `<table>
+								<thead>
+									<tr>
+										<th>Make</th><th>Model</th><th>Year</th><th>Color</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>{{Make}}</td><td>{{Model}}</td><td>{{Year}}</td><td>{{Color}}</td>
+									</tr>
+								</tbody>
+							</table>`
+})
+export class AppComponent {
+	Make: string = 'Toyota';
+	Model: string = 'Camry';
+	Year: string = '2016';
+	Color: string = 'Red';
+}
+
+
+/*
 @Component({
 	selector: 'my-app',
 	template: `<h1>{{header}}</h1>`
 })
-export class AppComponent {
+export class AppComponent1 {
 	header: string = 'My Colorful App';
 }
+*/
 
