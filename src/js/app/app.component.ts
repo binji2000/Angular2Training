e8775094ea344992da844906443855a202c22ef6
Bin Ji <bin.ji@wolterskluwer.com>
2016-10-10 14:21:33 -0400

    list of cars

--- src/js/app/app.component.ts
+++ src/js/app/app.component.ts
@@ -15,24 +15,66 @@ interface Car {
 	template: `<table>
 								<thead>
 									<tr>
-										<th>Make</th><th>Model</th><th>Year</th><th>Color</th>
+										<th>Make</th><th>Model</th><th>Year</th><th>Color;</th>
 									</tr>
 								</thead>
 								<tbody>
-									<tr *ngFor="let car of cars">
+									<tr *ngFor="let car of sortedByColor">
 										<td>{{car.make}}</td>
 										<td>{{car.model}}</td>
 										<td>{{car.year}}</td>
 										<td>{{car.color}}</td>
 									</tr>
 								</tbody>
-							</table>`
+							</table>
+							
+							<br/>
+
+							<div>
+							  <label>New Car Information:</label>
+								<br/>
+								<label for="new-make">Make</label>
+								<input type="text" id="new-make" name="new-make" [(ngModel)]="newCar.make" />
+								<label for="new-model">Model</label>
+								<input type="text" id="new-model" name="new-model" [(ngModel)]="newCar.model" />
+								<label for="new-year">Year</label>
+								<input type="text" id="new-year" name="new-year" [(ngModel)]="newCar.year" />
+								<label for="new-color">Color</label>
+								<input type="text" id="new-color" name="new-color" [(ngModel)]="newCar.color" />
+								<br/>
+								<button type="button" (click)="addCar()">Add Car</button>
+							</div>
+
+							
+							`
 })
 export class AppComponent {
+
+	newCar: Car = {make:'', model: '', year: 0, color:''};
+
 	cars: Car[] = [ 
 			{make: 'Toyota', model: 'Camry', year: 2016, color: 'Red'},
-			{make: 'Honda', model: 'Accord', year: 2014, color: 'Blue'}
+			{make: 'Honda', model: 'Accord', year: 2014, color: 'Blue'},
+			{make: 'Nissan', model: 'Sentra', year: 2010, color: 'White'}
 		];
+
+	addCar() {
+		this.cars.push(this.newCar);
+	}
+
+	get sortedByYear() : Car[] {
+		return  this.cars.concat().sort(
+			function(a:Car, b:Car){
+				return a.year - b.year
+			});
+	}
+
+	get sortedByColor() : Car[] {
+		return  this.cars.concat().sort(
+			function(a:Car, b:Car){
+				return (a.color<b.color?-1:(a.color>b.color?1:0))
+			});
+	}
 
 }
 
