83cdabe0164dc6990acce1b0ecb42830bb63e973
Bin Ji <bin.ji@wolterskluwer.com>
2016-10-11 14:05:22 -0400

    separated files

--- src/js/app/app.component.ts
+++ src/js/app/app.component.ts
@@ -7,52 +7,8 @@ import '../../css/styles.scss';
 
 @Component({
 	selector: 'my-app',
-	template: `
-						<div *ngIf="showTable">
-							<table>
-								<thead>
-									<tr>
-										<th>Make</th><th>Model</th><th>Year</th><th>Color;</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr *ngFor="let car of sortedByYear">
-										<td>{{car.make}}</td>
-										<td>{{car.model}}</td>
-										<td>{{car.year}}</td>
-										<td>{{car.color}}</td>
-									</tr>
-								</tbody>
-							</table>
-							
-							<br/>
-							<button type="button" (click)="startCreateCar()">Create</button>
-
-							<button type="button" (click)="sortAgain()">Sort</button>
-
-						</div>
-
-						<div *ngIf="!showTable">
-							<label>New Car Information:</label>
-							<br/>
-							<label for="new-make">Make</label>
-							<input type="text" id="new-make" name="new-make" [(ngModel)]="newCar.make" />
-							<label for="new-model">Model</label>
-							<input type="text" id="new-model" name="new-model" [(ngModel)]="newCar.model" />
-							<label for="new-year">Year</label>
-							<input type="text" id="new-year" name="new-year" [(ngModel)]="newCar.year" />
-							<label for="new-color">Color</label>
-							<input type="text" id="new-color" name="new-color" [(ngModel)]="newCar.color" />
-							<br/>
-							<button type="button" (click)="addCar(newCar)">Add Car</button> 
-						</div>
-
-						<input type="checkbox" [(ngModel)]="showTable" >Show Me
-						<div *ngIf="showTable">
-							you can see me!
-						</div>
-
-							`,
+	styles: [require ('./app.component.scss')],
+	template: require('./app.component.html'),
 	providers: [Cars]			
 })
 export class AppComponent {
@@ -61,6 +17,10 @@ export class AppComponent {
 	}
 
 	now: string = "";
+
+	favColor: string = "green";
+
+
 
 	newCar: Car = <Car>{};
 
