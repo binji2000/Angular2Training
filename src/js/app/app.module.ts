6358d509179a52f59749b3922197f22d10a447f2
Bin Ji <bin.ji@wolterskluwer.com>
2016-10-12 09:15:41 -0400

    cars2

--- src/js/app/app.module.ts
+++ src/js/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import '../../css/styles.scss';
-import { AppComponent, MyHeaderComponent } from './app.component';
+import { AppComponent,MyHeaderComponent } from './car2/app.component';
 
 @NgModule({
 	imports: [ BrowserModule, FormsModule ],
-	declarations: [ AppComponent, MyHeaderComponent ],
+	declarations: [ AppComponent,MyHeaderComponent ],
 	bootstrap: [ AppComponent ]
 })
 
